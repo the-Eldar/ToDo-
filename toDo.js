e42605ec9364e95d13beed7560d0a39f7eedf69a
Eldar <65613517+the-Eldar@users.noreply.github.com>
2020-07-01 14:05:02 +0500

    1

--- toDo.js
+++ toDo.js
@@ -3,13 +3,21 @@ const input = document.querySelector(".input-on");
 const list = document.querySelector(".list");
 let count = document.querySelector(".count");
 let countOne = 0;
+let listMass = [];
 
 btn.addEventListener("click", addList);
 
 function addList() {
-  let out = input.value;
-  list.innerHTML = input.value;
-  console.log(out);
+  let todoList = {
+    todo: input.value,
+    checked: false,
+  };
+
+  listMass.push(todoList);
+
+  list.innerHTML += `<li>${input.value}</li>`;
+  input.value = "";
+  console.log(listMass);
   countOne++;
   count.innerHTML = "ВСЕГО :" + countOne;
 }
