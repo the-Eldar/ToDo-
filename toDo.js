9bc2826a5a8664d799581b05791a0040e53fe20e
Eldar <65613517+the-Eldar@users.noreply.github.com>
2020-07-03 20:02:27 +0500

    done

--- toDo.js
+++ toDo.js
@@ -3,21 +3,32 @@ const input = document.querySelector(".input-on");
 const list = document.querySelector(".list");
 let count = document.querySelector(".count");
 let countOne = 0;
-let listMass = [];
 
 btn.addEventListener("click", addList);
 
 function addList() {
-  let todoList = {
-    todo: input.value,
-    checked: false,
-  };
-
-  listMass.push(todoList);
-
-  list.innerHTML += `<li>${input.value}</li>`;
-  input.value = "";
-  console.log(listMass);
+  showList(input.value);
+  console.log(input.value);
   countOne++;
   count.innerHTML = "ВСЕГО :" + countOne;
+  input.value = "";
+}
+
+function showList(value) {
+  const li = document.createElement("li");
+  li.className = "li";
+  li.textContent = value;
+
+  const closeBtn = document.createElement("closeBtn");
+  closeBtn.className = "closeBtn";
+  closeBtn.textContent = "x";
+  li.appendChild(closeBtn);
+
+  closeBtn.addEventListener("click", (e) => {
+    list.removeChild(li);
+    countOne--;
+    count.innerHTML = "ВСЕГО :" + countOne;
+  });
+
+  list.appendChild(li);
 }
